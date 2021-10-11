e34c80980b1c9bebbf0bf0aa3ea3a30fca861d48
Bishwajit Adhikary <bishwajitcadhikary@gmail.com>
2021-10-12 01:42:14 +0600

    Home to Dashboard

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Auth::routes(['register' => false]);
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', function() {
-    return view('home');
-})->name('home')->middleware('auth');
+Route::get('/dashboard', [App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard');
